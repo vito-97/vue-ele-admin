9666e07ac68d5e787b5dfee20c251d9e8eabd9d0
维多 <775113283@qq.com>
2023-02-28 15:53:48 +0800

    优化列表属性组件

--- src/views/custom/components/table-column/attrsEl.vue
+++ src/views/custom/components/table-column/attrsEl.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>
+  <div @dbclick.stop>
     <div v-for="(item,index) in val2array" :key="index">
-      <el-tag type="primary" effect="dark" size="mini">{{ item[opt.label] }}</el-tag>
-      <el-tag type="primary" size="mini" v-for="(it,idx) in item[opt.value]" :key="idx">{{ it }}</el-tag>
+      <template v-if="open || index < opt.preview_length">
+        <el-tag type="primary" effect="dark" size="mini" :disable-transitions="opt.disable_transitions">
+          {{ item[opt.label] }}
+        </el-tag>
+        <el-tag
+          type="primary"
+          size="mini"
+          :disable-transitions="opt.disable_transitions"
+          v-for="(it,idx) in item[opt.value]"
+          :key="idx">{{ it }}
+        </el-tag>
+      </template>
     </div>
+    <el-button
+      v-if="val2array.length > opt.preview_length"
+      type="text"
+      size="small"
+      class="toggle-btn"
+      @click="open = !open"
+      :icon="open ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"
+    >
+      {{ open ? '收起' : '展开' }}
+    </el-button>
   </div>
 </template>
 
@@ -15,9 +35,12 @@ export default {
   mixins: [tableColumnMixin],
   data() {
     return {
+      open: false,
       opts: {
         label: 'name',
-        value: 'list'
+        value: 'list',
+        preview_length: 3,
+        disable_transitions: true
       }
     }
   },
@@ -26,6 +49,10 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.toggle-btn {
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+}
 </style>
