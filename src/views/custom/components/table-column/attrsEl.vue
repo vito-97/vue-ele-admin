2dec1de616797d27a411555c42ab52f902756a08
维多 <775113283@qq.com>
2023-02-28 15:20:26 +0800

    新增列表的属性组件

---
+++ src/views/custom/components/table-column/attrsEl.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="(item,index) in val2array" :key="index">
+      <el-tag type="primary" effect="dark" size="mini">{{ item[opt.label] }}</el-tag>
+      <el-tag type="primary" size="mini" v-for="(it,idx) in item[opt.value]" :key="idx">{{ it }}</el-tag>
+    </div>
+  </div>
+</template>
+
+<script>
+import tableColumnMixin from './table-column-mixin'
+
+export default {
+  name: 'TagEl',
+  mixins: [tableColumnMixin],
+  data() {
+    return {
+      opts: {
+        label: 'name',
+        value: 'list'
+      }
+    }
+  },
+  filters: {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
